bbdcaad25f6633babca7558cb253188731885ddc
Gustavo Mustafa Mello Hanini <gustavo-mmello@hotmail.com>
2022-11-10 18:16:17 -0300

    Create Classes,Constructor, Tostring, Getters and Setters

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+import br.com.dio.desafio.dominio.Curso;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso de JAVA");
+        curso1.setDescricao("descrição Curso de JAVA");
+        curso1.setCargaHoraria(8);
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Curso de JS");
+        curso2.setDescricao("descrição Curso de JS");
+        curso2.setCargaHoraria(4);
+        System.out.println(curso1);
+        System.out.println(curso2);
     }
 }
